789db253b420f440b4570f3560356afb7d9754cc
santamav <117275531+santamav@users.noreply.github.com>
2023-11-07 19:33:07 +0100

    Update FollowLine.py
    MORE SPEEED!

--- Follow Line/FollowLine.py
+++ Follow Line/FollowLine.py
@@ -1,46 +1,60 @@
 from GUI import GUI
 from HAL import HAL
-
 import cv2
 
-i = 0
+# Constantes del control PID (ajusta estos valores según sea necesario)
+KP = 0.003  # Término proporcional
+KI = 0  # Término integral
+KD = 0 # Término derivativo
+
+linear_velocity = 3
+
+prev_error = 0
+integral = 0
+
 while True:
-    # Get view from the agent POV
+    # Obtener la vista desde el punto de vista del agente
     img = HAL.getImage()
-    
-    # Process the image to find the red line
+
+    # Procesar la imagen para encontrar la línea roja
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    red_mask = cv2.inRange(hsv, 
-                          (0, 125, 125), 
-                          (30, 255, 255))
-    
-    # Get the lines contours
-    contours, hierarchy = cv2.findContours(red_mask,
-                                          cv2.RETR_TREE,
-                                          cv2.CHAIN_APPROX_SIMPLE)
-    
-    # Find the centroid of the figure fromed by the contours
-    M = cv2.moments(contours[0])
-    
-    
-    # Calculate the error from the centroid to the center of the screen
-    if M["m00"] != 0:
-      cX = M["m10"] / M["m00"]
-      cY = M["m01"] / M["m00"]
-    else:
-      cX, cY = 0, 0
-      
-    # Calculate the velocity components of the agent
-      # Forward velocity (positive = forward) (negative = backwards)
-      # Angular velocity(positive = left) (negative = right)
-    if cX > 0:
-      err = 320 - cX
-      HAL.setV(1)
-      HAL.setW(0.01 * err)
-      
-      
-    # Show agent POV
+    red_mask = cv2.inRange(hsv, (0, 125, 125), (30, 255, 255))
+
+    # Obtener los contornos de las líneas
+    contours, hierarchy = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Encontrar el centroide de la figura formada por los contornos
+    if len(contours) > 0:
+        M = cv2.moments(contours[0])
+        if M["m00"] != 0:
+            cX = M["m10"] / M["m00"]
+            cY = M["m01"] / M["m00"]
+        else:
+            cX, cY = 0, 0
+
+        # Calcular el error desde el centroide hasta el centro de la pantalla
+        error = 320 - cX
+
+        # Calcular los términos del control PID
+        P = KP * error
+        integral += error
+        I = KI * integral
+        derivative = error - prev_error
+        D = KD * derivative
+
+        
+        if(cX > 0):
+          # Calcular la velocidad angular del agente usando control PID
+          angular_velocity = P + I + D
+  
+          # Establecer las velocidades del agente
+          HAL.setV(linear_velocity)  # Velocidad hacia adelante constante
+          HAL.setW(angular_velocity)
+
+        prev_error = error
+
+    # Mostrar la vista del agente
     GUI.showImage(red_mask)
-    # Update number of steps
-    print('½d cX: %.2f cY: %.2f')
-    i = i + 1
+
+    # Actualizar el número de pasos
+    print('cX: %.2f cY: %.2f' % (cX, cY))
