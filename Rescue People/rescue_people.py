fc499a7b45f814e1eaa7655d2a5bec2b35c7d880
santamav <117275531+santamav@users.noreply.github.com>
2023-11-28 19:29:38 +0100

    Update rescue_people.py
    * Spiral movement
      - There are still errors with the spiral movement

--- Rescue People/rescue_people.py
+++ Rescue People/rescue_people.py
@@ -1,5 +1,6 @@
 from GUI import GUI
 from HAL import HAL
+import cv2
 
 # Coordinates of the safety boat and known survivor location
 boat_coordinates = (430492, 4459162)  # 40º16'48.2" N, 3º49'03.5" W
@@ -15,27 +16,55 @@ takeoff_height = 4
 
 x_vel = 0.25
 angle = 0.6
-iterations = 0
-spiral_iterations = 300
-landing_margin = 0.07
 
 x_pos = HAL.get_position()[0]
 y_pos = HAL.get_position()[1]
 
+initial_linear_vel = 3 # Meters per second
+linear_vel = initial_linear_vel # Meters per second
+linear_vel_inc = 1# Meters per second
+
+ang_vel = 0.79 # Radians per second
+
+
+num_victims = 5
+saved_victims = 0
+faces = []
+
 # Takeoff
 HAL.takeoff(takeoff_height)
 # Move to accident position
-while not ((victims_x-1 < x_pos) and (x_pos <victims_x+1) and (victims_y-1 < y_pos) and (y_pos < victims_y+1)):
-	GUI.showImage(HAL.get_frontal_image())
-	GUI.showLeftImage(HAL.get_ventral_image())
-	x_pos = HAL.get_position()[0]
-	y_pos = HAL.get_position()[1]
-	HAL.set_cmd_pos(victims_x, victims_y, 3, angle)
-	# TODO: Should calculate the angle to face the direction in which we are moving
-	# time.sleep(0.01)
-# Find and save 
 
+inPosition = True
+while inPosition:
+  GUI.showImage(HAL.get_frontal_image())
+  GUI.showLeftImage(HAL.get_ventral_image())
+  x_pos = HAL.get_position()[0]
+  y_pos = HAL.get_position()[1]
+  HAL.set_cmd_pos(victims_x, victims_y, 3, angle)
+  # TODO: Should calculate the angle to face the direction in which we are moving
+  # time.sleep(0.01)
+  if ((victims_x-1 < x_pos) and (x_pos <victims_x+1) and (victims_y-1 < y_pos) and (y_pos < victims_y+1)):
+    inPosition = False
+
+	
+# Find and save 
+while (saved_victims <= num_victims):
+  # Get cameras data
+  frontal_img = HAL.get_frontal_image()
+  ventral_img = HAL.get_ventral_image()
+  # Show images
+  GUI.showImage(frontal_img)
+  GUI.showLeftImage(ventral_img)
+  # Check if there are any faces
+
+  # If no faces move in spiral
+  # increment linear velocity
+  linear_vel += linear_vel_inc
+  HAL.set_cmd_mix(linear_vel, 0, takeoff_height, ang_vel)
+
+# Final Loop
 while True:
-  	# Enter iterative code!
-    GUI.showImage(HAL.get_frontal_image())
-    GUI.showLeftImage(HAL.get_ventral_image())
+  # Enter iterative code!
+  GUI.showImage(HAL.get_frontal_image())
+  GUI.showLeftImage(HAL.get_ventral_image())
