3a35fbd3598c58fdf209644e2ebcebc75e32863c
vicentamen <santamav@uji.es>
2023-12-19 10:41:48 +0100

    DETECTA CARAS!

--- Rescue People/rescue_people.py
+++ Rescue People/rescue_people.py
@@ -1,8 +1,6 @@
 from GUI import GUI
 from HAL import HAL
 # Enter sequential code!
-
-from HAL import HAL
 import cv2 as cv
 
 # Coordinates of the safety boat and known survivor location
@@ -15,7 +13,7 @@ print("x: ", victims_x, "// y: ", victims_y)
 boat_x = 0
 boat_y = 0
 
-takeoff_height = 3
+takeoff_height = 2
 
 x_vel = 0.25
 angle = 0.6
@@ -29,7 +27,7 @@ linear_vel_inc = 0.00005# Meters per second
 
 ang_vel = 0.79 # Radians per second
 
-face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_alt.xml')
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 
 num_victims = 5
@@ -47,11 +45,17 @@ while inPosition:
   x_pos = HAL.get_position()[0]
   y_pos = HAL.get_position()[1]
   HAL.set_cmd_pos(victims_x, victims_y, 3, angle)
-  # TODO: Should calculate the angle to face the direction in which we are moving
   # time.sleep(0.01)
   if ((victims_x-1 < x_pos) and (x_pos <victims_x+1) and (victims_y-1 < y_pos) and (y_pos < victims_y+1)):
     inPosition = False
 
+
+def VictimFound(victim_location):
+  # Check if the location of the victim is already on the dictionary
+  print("victim_location", victim_location)
+  # If not, add a new found victim
+  # If true, continuw with the search
+  return False
 	
 # Find and save 
 while (saved_victims <= num_victims):
@@ -61,14 +65,26 @@ while (saved_victims <= num_victims):
   # Show images
   GUI.showImage(frontal_img)
   GUI.showLeftImage(ventral_img)
-  # Check if there are any faces
+  # Transform the image to grayscale
   img_gray = cv.cvtColor(ventral_img, cv.COLOR_BGR2GRAY)
   for angle in range (0, 365, 10):
-    detected_faces = face_cascade.detectMultiScale(cv.rotate(img_gray, angle), 1.1, 4)
-    print("Detected faces", detected_faces)
-    # When there is a face stop on top of it and recognize it
-    # If no faces move in spiral
-    # increment linear velocity
+    # Compute rotation matrix
+    (h, w) = img_gray.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv.getRotationMatrix2D(center, angle, 1.0)
+
+    # Perform the rotation
+    im_rot = cv.warpAffine(img_gray, M, (w, h))
+    #plt.imshow(im_rot, cmap='gray')
+    #plt.show()
+
+    # Detect faces
+    detected_faces = face_cascade.detectMultiScale(im_rot, 1.1, 4)
+    #print("Detected faces at angle", angle, ":", detected_faces)
+    # When if we detect a face, store it location and exit the loop
+    for face in detected_faces:
+      if(VictimFound(face)): break
+
   linear_vel += linear_vel_inc
   HAL.set_cmd_mix(linear_vel, 0, takeoff_height, ang_vel)
 
